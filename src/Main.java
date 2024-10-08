d3a23b8bf888e8f4daf176b404b289e9eb4ae825
NaSuRal <romain.poulain@edu.esiee-it.fr>
2024-10-08 09:12:28 +0200

    Add Tp1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        int[] scores = new int[7];
+        scores[0] = 10;
+        scores[1] = 20;
+        scores[2] = 30;
+        scores[3] = 40;
+        scores[4] = 50;
+        scores[5] = 60;
+        scores[6] = 70;
+
+        for (int i = 0; i < scores.length; i++) {
+            int s = scores[i];
+            System.out.println(s);
+        }
+
+       int Moyenne =  scores[0] + scores[1] + scores[2] + scores[3] + scores[4] + scores[5] + scores[6];
+        System.out.println(Moyenne);
+        int div = Moyenne / 7;
+        System.out.println(div);
     }
+
 }
